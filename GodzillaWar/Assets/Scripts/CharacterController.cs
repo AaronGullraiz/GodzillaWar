4772dfa3857788fee33047e0812fc5725586db7e
AaronGullraiz <aarongullraiz@gmail.com>
2022-10-01 14:41:02 +0500

    final push
    UFE removed
    Final controls

--- GodzillaWar/Assets/Scripts/CharacterController.cs
+++ GodzillaWar/Assets/Scripts/CharacterController.cs
@@ -8,14 +8,96 @@ public class CharacterController : MonoBehaviour
     public Animator anim;
     public Rigidbody rigidBody;
     public float speed;
+    public float speedRot;
+    public float speedMove;
+    public ParticleSystem particle;
+
+    public AudioClip[] hitClips;
+    public AudioClip[] strafClips;
+
+    private bool isAttacking;
+
+    private float attackDelay = 0;
+
+    private EnemyController enemyController;
+
+    private AudioSource source;
 
     void Start()
     {
+        enemyController = enemy.GetComponent<EnemyController>();
         anim = GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody>();
+        source = GetComponent<AudioSource>();
     }
 
     void Update()
+    {
+        float y = Input.GetAxis("P1KeyboardHorizontal");
+        float x = Input.GetAxis("P1KeyboardVertical");
+        Move(x, y);
+        Attack();
+
+        if (isAttacking)
+        {
+            if (attackDelay < 0)
+            {
+                isAttacking = false;
+            }
+            attackDelay -= Time.deltaTime;
+        }
+    }
+
+    private void Move(float x, float y)
+    {
+        if (!isAttacking)
+        {
+            var rot = enemy.localEulerAngles;
+            rot.y += x * speedRot / Vector3.Distance(transform.position, enemy.position);
+            enemy.localEulerAngles = rot;
+
+            rigidBody.velocity = transform.forward * y * speedMove;
+
+            anim.SetFloat("Walk", Mathf.Clamp01(Mathf.Abs(x) + Mathf.Abs(y)));
+        }
+    }
+
+    private void Attack()
+    {
+        if (!isAttacking)
+        {
+            if (Input.GetKeyDown(KeyCode.U))
+            {
+                isAttacking = true;
+                attackDelay = 1;
+                anim.SetTrigger("Punch1");
+                enemyController.Straf();
+            }
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                isAttacking = true;
+                attackDelay = 1;
+                anim.SetTrigger("Punch2");
+                enemyController.Straf();
+            }
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                isAttacking = true;
+                attackDelay = 1;
+                anim.SetTrigger("Kick1");
+                enemyController.Straf();
+            }
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                isAttacking = true;
+                attackDelay = 1;
+                anim.SetTrigger("Kick2");
+                enemyController.Straf();
+            }
+        }
+    }
+
+    private void OldControls()
     {
         Vector3 movement = new Vector3(
             Input.GetAxis("P1KeyboardHorizontal"),
@@ -31,5 +113,19 @@ public class CharacterController : MonoBehaviour
         var rot = transform.localEulerAngles;
         rot.x = rot.z = 0;
         transform.localEulerAngles = rot;
+    }
+
+    public void PlayHitEffect()
+    {
+        var dist = Vector3.Distance(enemy.position, transform.position);
+        if (dist < 8)
+        {
+            source.PlayOneShot(hitClips[Random.Range(0, hitClips.Length)]);
+            particle.Play();
+        }
+        else
+        {
+            source.PlayOneShot(strafClips[Random.Range(0, strafClips.Length)]);
+        }
     }
 }
--- GodzillaWar/Assets/Scripts/EnemyController.cs
+++ GodzillaWar/Assets/Scripts/EnemyController.cs
@@ -6,16 +6,37 @@ public class EnemyController : MonoBehaviour
 {
     public Transform player;
 
+    private Animator anim;
+
+    private float strafTime = 0;
+
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
-    void Update()
+    //void Update()
+    //{
+    //    transform.LookAt(player);
+    //    var rot = transform.localEulerAngles;
+    //    rot.x = rot.z = 0;
+    //    transform.localEulerAngles = rot;
+    //}
+    private void Update()
     {
-        transform.LookAt(player);
-        var rot = transform.localEulerAngles;
-        rot.x = rot.z = 0;
-        transform.localEulerAngles = rot;
+        if (strafTime > 0)
+        {
+            strafTime -= Time.deltaTime;
+        }
+    }
+
+    public void Straf()
+    {
+        var dist = Vector3.Distance(player.position, transform.position);
+        if (dist < 8 && strafTime <= 0)
+        {
+            strafTime = 2;
+            anim.SetTrigger("Straf");
+        }
     }
 }
